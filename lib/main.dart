3c14c3498286728635becee929146f468e769ed5
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-08-06 16:47:28 +0330

    Initial App

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'Next Screen',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
